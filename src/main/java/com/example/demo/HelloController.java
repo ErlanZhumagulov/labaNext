9f569efc1e16b703268cfc8e6cbcf0f5a8569d0d
Ерлан <erlanrulit@mail.ru>
2024-03-29 12:51:12 +0400

    Control chek!!!

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @GetMapping("/api/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("HELLO!!!!11!!!");
+        return ResponseEntity.ok("HELLO!!!!11!!!!");
     }
 }
